f700b5227dd716a57a25c501e5fd8c2d22ddb629
Cyril ALFARO <cyril@dbyzero.com>
2024-02-20 00:22:25 +0100

    fix: switch from employee list to report

--- models/hris_hrisuser.sql
+++ models/hris_hrisuser.sql
@@ -5,38 +5,38 @@
 ) }}
 
 SELECT 
-    DISTINCT "{{ var("table_prefix") }}_employees_directory_stream".id as external_id,
+    DISTINCT "{{ var("table_prefix") }}_custom_reports_stream".id as external_id,
     NOW() as created,
     NOW() as modified,
     md5(
         '{{ var("integration_id") }}' ||
-        "{{ var("table_prefix") }}_employees_directory_stream".id ||
+        "{{ var("table_prefix") }}_custom_reports_stream".id ||
         'user' ||
         'bamboohr'
     )  as id,
     'bamboohr' as source,
     '{{ var("integration_id") }}'::uuid  as integration_id,
-    _airbyte_raw_{{ var("table_prefix") }}_employees_directory_stream._airbyte_data as last_raw_data, 
+    _airbyte_raw_{{ var("table_prefix") }}_custom_reports_stream._airbyte_data as last_raw_data, 
     '{{ var("timestamp") }}' as sync_timestamp,
-    "{{ var("table_prefix") }}_employees_directory_stream".workemail as email,
-    "{{ var("table_prefix") }}_employees_directory_stream".firstname as firstname,
+    "{{ var("table_prefix") }}_custom_reports_stream".workemail as email,
+    "{{ var("table_prefix") }}_custom_reports_stream".firstname as firstname,
     NULL as birth_date,
-    "{{ var("table_prefix") }}_employees_directory_stream".lastname as lastname,
-    "{{ var("table_prefix") }}_employees_directory_stream".mobilephone as phone_number,
+    "{{ var("table_prefix") }}_custom_reports_stream".lastname as lastname,
+    "{{ var("table_prefix") }}_custom_reports_stream".mobilephone as phone_number,
     NULL as language,
-    "{{ var("table_prefix") }}_employees_directory_stream".division as legal_entity_name,
-    "{{ var("table_prefix") }}_employees_directory_stream".supervisorEmail as manager_email,
-    "{{ var("table_prefix") }}_employees_directory_stream".jobtitle as job_title,
-    "{{ var("table_prefix") }}_employees_directory_stream".division as team,
-    "{{ var("table_prefix") }}_employees_directory_stream".location as country,
-    NULL as contract_start_date,
-    NULL as contract_end_date,
-    "{{ var("table_prefix") }}_employees_directory_stream".pronouns as gender,
-    "{{ var("table_prefix") }}_employees_directory_stream".division as business_unit,
-    "{{ var("table_prefix") }}_employees_directory_stream".employeeNumber as employee_number,
-    "{{ var("table_prefix") }}_employees_directory_stream".employmentHistoryStatus as contract_type,
-    NULL as socio_professional_category,
-    NULL as state
-FROM "{{ var("table_prefix") }}_employees_directory_stream"
-LEFT JOIN _airbyte_raw_{{ var("table_prefix") }}_employees_directory_stream
-    ON "{{ var("table_prefix") }}_employees_directory_stream"._airbyte_ab_id = "_airbyte_raw_{{ var("table_prefix") }}_employees_directory_stream"._airbyte_ab_id
+    "{{ var("table_prefix") }}_custom_reports_stream".division as legal_entity_name,
+    "{{ var("table_prefix") }}_custom_reports_stream".supervisorEmail as manager_email,
+    "{{ var("table_prefix") }}_custom_reports_stream".jobtitle as job_title,
+    "{{ var("table_prefix") }}_custom_reports_stream".division as team,
+    "{{ var("table_prefix") }}_custom_reports_stream".country as country,
+    "{{ var("table_prefix") }}_custom_reports_stream".hireDate as contract_start_date,
+    "{{ var("table_prefix") }}_custom_reports_stream".terminationDate as contract_start_date,
+    "{{ var("table_prefix") }}_custom_reports_stream".gender as gender,
+    "{{ var("table_prefix") }}_custom_reports_stream".division as business_unit,
+    "{{ var("table_prefix") }}_custom_reports_stream".employeeNumber as employee_number,
+    "{{ var("table_prefix") }}_custom_reports_stream".employmentHistoryStatus as contract_type,
+    "{{ var("table_prefix") }}_custom_reports_stream".department as socio_professional_category,
+    "{{ var("table_prefix") }}_custom_reports_stream".state as state ,
+FROM "{{ var("table_prefix") }}_custom_reports_stream"
+LEFT JOIN _airbyte_raw_{{ var("table_prefix") }}_custom_reports_stream
+    ON "{{ var("table_prefix") }}_custom_reports_stream"._airbyte_ab_id = "_airbyte_raw_{{ var("table_prefix") }}_custom_reports_stream"._airbyte_ab_id
